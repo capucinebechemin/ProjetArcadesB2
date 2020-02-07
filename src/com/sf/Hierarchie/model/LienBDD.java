76b1264f59d4ea92d35cadbf817913dbb7e7f714
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-02-07 11:24:05 +0100

    Nouvelles fonctions de connexion à la BDD

--- src/com/sf/Hierarchie/model/LienBDD.java
+++ src/com/sf/Hierarchie/model/LienBDD.java
@@ -2,7 +2,6 @@ package com.sf.Hierarchie.model;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Properties;
 
 public class LienBDD {
 
@@ -45,19 +44,21 @@ public class LienBDD {
         // ------------------------------
         return liste;
     }
+
+
     public static void update(Personnage p) {
-        String requete = "UPDATE personnage SET points_vie = ?,puissance_tir =?, delai_tir =?, vitesse_deplacement =?, vitesse_rotation =?, vitesse_projectile =? WHERE id=?";
+        String requete = "UPDATE personnage SET points_vie = ?,puissance_tir =?, vitesse_deplacement =?, vitesse_projectile =? WHERE id=?";
         Connection connection = getConnection();
         try {
             PreparedStatement prepare = (PreparedStatement) connection.prepareStatement(requete);
             // je viens binder le 1er ?
             prepare.setInt(1, p.getPoints_vie());
             prepare.setInt(2, p.getPuissance_tir());
-            prepare.setInt(3, p.getDelai_tir());
-            prepare.setInt(4, p.getVitesse_personnage());
-            prepare.setInt(5, p.getVitesse_rotaion());
-            prepare.setInt(6, p.getVitesse_projectil());
-            prepare.setInt(7, p.getId());
+//            prepare.setInt(3, p.getDelai_tir());
+            prepare.setInt(3, p.getVitesse_personnage());
+//            prepare.setInt(5, p.getVitesse_rotaion());
+            prepare.setInt(4, p.getVitesse_projectil());
+            prepare.setInt(5, p.getId());
             prepare.execute();
             prepare.close();
             connection.commit();
@@ -68,4 +69,101 @@ public class LienBDD {
         }
     }
 
+
+    public static Integer selectVitesse(int i) {
+
+        Connection connection = getConnection();
+        // ------------------------------
+        String requete = "SELECT vitesse_deplacement FROM personnage WHERE id =" + i;
+        int vitesse = 0;
+        try {
+            Statement state ;
+            ResultSet result ;
+
+
+            state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = state.executeQuery(requete);
+
+            while (result.next()) {
+                vitesse = result.getInt("vitesse_deplacement");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return vitesse;
+    }
+
+
+    public static Integer selectLife(int i) {
+
+        Connection connection = getConnection();
+        // ------------------------------
+        String requete = "SELECT points_vie FROM personnage WHERE id =" + i;
+        int life = 0;
+        try {
+            Statement state ;
+            ResultSet result ;
+
+
+            state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = state.executeQuery(requete);
+
+            while (result.next()) {
+                life = result.getInt("points_vie");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return life;
+    }
+
+    public static Integer selectPuissance(int i) {
+
+        Connection connection = getConnection();
+        // ------------------------------
+        String requete = "SELECT puissance_tir FROM personnage WHERE id =" + i;
+        int power = 0;
+        try {
+            Statement state ;
+            ResultSet result ;
+
+
+            state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = state.executeQuery(requete);
+
+            while (result.next()) {
+                power = result.getInt("puissance_tir");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return power;
+    }
+
+    public static Integer selectVitesseTir(int i) {
+
+        Connection connection = getConnection();
+        // ------------------------------
+        String requete = "SELECT vitesse_projectile FROM personnage WHERE id =" + i;
+        int speed_shoot = 0;
+        try {
+            Statement state ;
+            ResultSet result ;
+
+
+            state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = state.executeQuery(requete);
+
+            while (result.next()) {
+                speed_shoot = result.getInt("vitesse_projectile");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return speed_shoot;
+    }
 }
